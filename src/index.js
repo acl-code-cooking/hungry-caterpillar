6cf61dc09130774434670473977dccaf24cde7d8
Easton Gorishek <ieaston26@gmail.com>
2019-01-30 16:31:33 -0800

    created color blocks and appended to canvas

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const colors = [
 ];
 
 const colorButtons = document.getElementById('color-buttons');
+const canvas = document.getElementById('paint-canvas');
 
 for(let index = 0; index < colors.length; index++) {
     let color = colors[index];
@@ -16,10 +17,18 @@ for(let index = 0; index < colors.length; index++) {
 
     colorButton.addEventListener('click', function() {
         console.log('I\'ve been clicked!');
+        paint(color);
     });
 
     colorButtons.appendChild(colorButton);
 }
 
+function paint(color) {
+    const colorBlock = document.createElement('span');
+    colorBlock.classList.add(color);
+    colorBlock.classList.add('color-block');
+
+    canvas.appendChild(colorBlock);
+}
 
 
