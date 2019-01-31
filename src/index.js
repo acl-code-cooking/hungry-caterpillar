59473fd7def933f52bcc2aeefe95998587177e5f
Easton Gorishek <ieaston26@gmail.com>
2019-01-30 17:13:28 -0800

    added resetShape function

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const colors = [
 
 const colorButtons = document.getElementById('color-buttons');
 const canvas = document.getElementById('paint-canvas');
+const transformButtons = document.getElementById('transform-buttons');
+
+// loop creates color buttons
 
 for(let index = 0; index < colors.length; index++) {
     let color = colors[index];
@@ -16,11 +19,27 @@ for(let index = 0; index < colors.length; index++) {
     colorButton.textContent = color;
 
     colorButton.addEventListener('click', function() {
-        console.log('I\'ve been clicked!');
         paint(color);
     });
 
     colorButtons.appendChild(colorButton);
+}
+
+// transform loop 
+
+for(let index = 0; index < colors.length; index++) {
+    let color = colors[index];
+    const transformButton = document.createElement('button');
+    transformButton.value = color;
+
+    transformButton.textContent = color;
+
+    transformButton.addEventListener('click', function() {
+        resetShape();
+        circleTransform(color);
+    });
+
+    transformButtons.appendChild(transformButton);
 }
 
 function paint(color) {
@@ -31,4 +50,20 @@ function paint(color) {
     canvas.appendChild(colorBlock);
 }
 
+function circleTransform(color) {
+    const selector = '.color-block.' + color;
+    const colorBlocks = document.querySelectorAll(selector);
+
+    for(let index = 0; index < colorBlocks.length; index++) {
+        colorBlocks[index].classList.add('transform');
+    }
+}
+
+function resetShape() {
+    const colorBlocks = document.querySelectorAll('.color-block');
+
+    for(let index = 0; index < colorBlocks.length; index++) {
+        colorBlocks[index].classList.remove('transform');
+    }
+}
 
